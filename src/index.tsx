3ebd7471c93e5a5aed44d8b1cfe2c861e1e61624
Vlad_Raginya <vladoserr37@gmail.com>
2021-03-11 14:37:01 +0300

    Added redux to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './app/store';
 import GlobalStyles from './globalStyles';
 import App from './app/App';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
